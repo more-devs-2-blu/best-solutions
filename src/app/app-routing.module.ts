c8c171bffe23367f6d5aa4ace9c2c2a92ad1ef03
cuelhosandra <sandracoelho_br@hotmail.com>
2023-03-15 14:56:50 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { EtapasComponent } from './pages/etapas/etapas.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EtapasComponent } from './etapas/etapas.component';
+import { CadastrarEmpresaComponent } from './cadastrar-empresa/cadastrar-empresa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'about', component:AboutComponent},
+  {path:'etapas', component:EtapasComponent},
+  {path:'cadastrarEmpresa', component:CadastrarEmpresaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
+
+import { TableModule } from 'primeng/table';
+
+import { MegaMenuModule } from 'primeng/megamenu';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+// import { EtapasComponent } from './pages/etapas/etapas.component'
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EtapasComponent } from './etapas/etapas.component';
+import { CadastrarEmpresaComponent } from './cadastrar-empresa/cadastrar-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    AboutComponent,
+    CadastrarEmpresaComponent,
+    EtapasComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ButtonModule,
+    MegaMenuModule,
+    RouterModule,
+    AppRoutingModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
