bc15e1e1b4e07b6f5a3af190eea8dcf6bcbb4d2d
cuelhosandra <sandracoelho_br@hotmail.com>
2023-03-15 16:46:18 -0300

    ajuste e exclusao de algumas pags

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
-import { MenuComponent } from './pages/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EtapasComponent } from './pages/etapas/etapas.component';
 import { CadastrarEmpresaComponent } from './pages/cadastrar-empresa/cadastrar-empresa.component';
@@ -9,7 +8,6 @@ import { TelaDeCadastroComponent } from './pages/tela-de-cadastro/tela-de-cadast
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'menu', component:MenuComponent},
   {path:'about', component:AboutComponent},
   {path:'etapas', component:EtapasComponent},
   {path:'cadastrarEmpresa', component:CadastrarEmpresaComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 
 import { TableModule } from 'primeng/table';
-
 import { MegaMenuModule } from 'primeng/megamenu';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
-import { MenuComponent } from './pages/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-// import { EtapasComponent } from './pages/etapas/etapas.component'
-
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,7 +19,6 @@ import { TelaDeCadastroComponent } from './pages/tela-de-cadastro/tela-de-cadast
   declarations: [
     AppComponent,
     NavbarComponent,
-    MenuComponent,
     HomeComponent,
     AboutComponent,
     CadastrarEmpresaComponent,
